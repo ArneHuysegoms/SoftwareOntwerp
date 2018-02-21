dfd9318297d038da3d1a9b7ac449834d77c8883d
laurensvangoubergen <laurens.vangoubergen@student.kuleuven.be>
2018-02-21 16:24:45 +0100

    Test

--- UI/src/main/java/canvas/CanvasWindow.java
+++ UI/src/main/java/canvas/CanvasWindow.java
@@ -33,7 +33,7 @@ import javax.swing.JPanel;
  * To use this class, create a subclass, say MyCanvasWindow, that overrides
  * methods {@link #paint(Graphics)}, {@link #handleMouseEvent(MouseEvent)}, and {@link #handleKeyEvent(KeyEvent)}, and then launch
  * it from your main method as follows:
- *
+ * 
  * <pre>
  * public static void main(String[] args) {
  *     java.awt.EventQueue.invokeLater(() -> {
