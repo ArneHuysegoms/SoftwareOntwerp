db482acf03439b77e02a31e9ec41ce0895816bfb
AdamBoghe <adam.boghe@gmail.com>
2018-02-26 15:51:53 +0100

    figure start - interfaces

---
+++ UI/src/main/java/figures/IDrawable.java
@@ -0,0 +1,10 @@
+package figures;
+
+import java.awt.*;
+
+/**
+ * Created by Yewsi on 26-Feb-18.
+ */
+public interface IDrawable {
+    public void draw(Graphics graphics);
+}
---
+++ UI/src/main/java/figures/basicShapes/Shape.java
@@ -0,0 +1,10 @@
+package figures.basicShapes;
+
+import figures.IDrawable;
+
+/**
+ * Created by Yewsi on 26-Feb-18.
+ */
+public abstract class Shape implements IDrawable {
+
+}
