bb4f744489c24843d86f14bf118af27f3d553fcd
Sander Joos <sander.joos@skynet.be>
2018-02-28 16:13:08 +0100

    added lifeline, and made elements clickable and findable

---
+++ Domain/src/main/java/canvascomponents/diagram/Lifeline.java
@@ -0,0 +1,48 @@
+package canvascomponents.diagram;
+
+import canvascomponents.Clickable;
+import canvascomponents.Coordinable;
+
+import java.awt.geom.Point2D;
+import java.util.List;
+
+public class Lifeline implements Clickable, Coordinable {
+
+    private Point2D coordinate;
+    private List<Message> messages;
+
+    public Lifeline(List<Message> messages, Point2D coordinate) {
+        this.setMessages(messages);
+        this.setCoordinate(coordinate);
+    }
+
+    @Override
+    public Point2D getCoordinate() {
+        return coordinate;
+    }
+
+    private void setCoordinate(Point2D coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    private void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public void addMessage(Message message){
+        this.getMessages().add(message);
+    }
+
+    private void removeMessage(Message message){
+        this.removeMessage(message);
+    }
+
+    @Override
+    public boolean isClicked(Point2D point2D) {
+        return false;
+    }
+}
