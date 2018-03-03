706d641d4b24434984f87e85fce410a4f148f6be
Sander Joos <sander.joos@skynet.be>
2018-03-03 11:51:32 +0100

    canvasmakeup

--- UI/src/main/java/canvas/CanvasMakeUp.java
+++ UI/src/main/java/canvas/CanvasMakeUp.java
@@ -1,6 +1,59 @@
 package canvas;
 
+import canvascomponents.diagram.CommunicationsDiagram;
+import canvascomponents.diagram.Diagram;
+import canvascomponents.diagram.SequenceDiagram;
+import uievents.KeyEvent;
+import uievents.MouseEvent;
+
+import java.awt.geom.Point2D;
+
 public class CanvasMakeUp {
 
+    private Diagram activeDiagram;
 
+    private Diagram communicationDiagram;
+    private Diagram sequenceDiagram;
+
+    public CanvasMakeUp(){
+        communicationDiagram = new CommunicationsDiagram();
+        communicationDiagram.addNewParty(new Point2D.Double(25, 25));
+
+        sequenceDiagram = new SequenceDiagram();
+        sequenceDiagram.addNewParty(new Point2D.Double(25, 51));
+
+        activeDiagram = sequenceDiagram;
+
+    }
+
+    public Diagram getActiveDiagram() {
+        return activeDiagram;
+    }
+
+    private void setActiveDiagram(Diagram activeDiagram) {
+        this.activeDiagram = activeDiagram;
+    }
+
+    public void changeActiveDiagram(){
+        if(activeDiagram == communicationDiagram){
+            activeDiagram = sequenceDiagram;
+        }
+        else{
+            activeDiagram = communicationDiagram;
+        }
+    }
+
+    public void handleKeyEvent(KeyEvent keyEvent){
+
+    }
+
+    public void handleMouseEvent(MouseEvent mouseEvent){
+        if(! checkIfInLabelMode()){
+
+        }
+    }
+
+    private boolean checkIfInLabelMode(){
+        return this.activeDiagram.isLabelMode();
+    }
 }
