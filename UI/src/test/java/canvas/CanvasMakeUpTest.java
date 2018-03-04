1ea8dd9dc4033ab225962541b1ba99ecb919e90d
Unknown <arhuy@hotmail.com>
2018-03-04 18:33:20 +0100

    Added BACKSPACE to KeyEvents Added CanvasMakeUpTest

--- UI/src/test/java/canvas/CanvasMakeUpTest.java
+++ UI/src/test/java/canvas/CanvasMakeUpTest.java
@@ -1,7 +1,11 @@
 package canvas;
+import canvascomponents.diagram.CommunicationsDiagram;
+import canvascomponents.diagram.Diagram;
 import canvascomponents.diagram.SequenceDiagram;
 import org.junit.Before;
 import org.junit.Test;
+import uievents.KeyEvent;
+import uievents.KeyEventType;
 
 import java.awt.*;
 
@@ -18,6 +22,24 @@ public class CanvasMakeUpTest {
     @Test
     public void activeDiagram_is_sequence_after_init(){
         assertTrue(canvasMakeUp.getActiveDiagram() instanceof SequenceDiagram);
+    }
+
+    @Test
+    public void test_changeActiveDiagram(){
+        canvasMakeUp.changeActiveDiagram();
+        assertTrue(canvasMakeUp.getActiveDiagram() instanceof CommunicationsDiagram);
+    }
+
+    @Test
+    public void test_handleKeyEvent(){
+        Diagram d = canvasMakeUp.getActiveDiagram();
+        KeyEventType type = KeyEventType.TAB;
+        KeyEvent keyEvent = new KeyEvent(type);
+        canvasMakeUp.handleKeyEvent(keyEvent);
+        Diagram d2 = canvasMakeUp.getActiveDiagram();
+        System.out.print(d.getClass());
+        System.out.print(d2.getClass());
+        assertNotEquals(d.getClass(), d2.getClass());
     }
 
 }
