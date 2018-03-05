2034895c3f43cfb82f8a319565dc463c50434f3b
Unknown <arhuy@hotmail.com>
2018-03-05 11:29:29 +0100

    Aanmaken tests shapes

---
+++ UI/src/test/java/basicShapes/CircleTest.java
@@ -0,0 +1,16 @@
+package basicShapes;
+
+import figures.basicShapes.Circle;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CircleTest {
+    private Circle circle;
+    @Before
+    public void setUp(){
+        circle = new Circle(5,5,3);
+    }
+
+    @Test
+    public void 
+}
---
+++ UI/src/test/java/basicShapes/LineTest.java
@@ -0,0 +1,4 @@
+package basicShapes;
+
+public class LineTest {
+}
