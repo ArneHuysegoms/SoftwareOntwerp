30806e6afa4869c9c78d63a265f96f9379368000
Unknown <arhuy@hotmail.com>
2018-03-08 18:05:13 +0100

    UI Tests!

--- UI/src/test/java/AllTests.java
+++ UI/src/test/java/AllTests.java
@@ -3,6 +3,7 @@ import figures.diagramFigures.*;
 import figures.helperClasses.*;
 import uievents.*;
 import canvas.*;
+import canvaswindow.*;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -24,6 +25,7 @@ import org.junit.runners.Suite;
         BoxTest.class,
         ArrowTest.class,
         DashedArrowTest.class,
+        InteractrCanvasTest.class,
 
 
 })
