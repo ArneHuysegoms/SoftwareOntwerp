a2dd0dd6cb9c03a05f440d4d1c646add8a12635c
AdamBoghe <adam.boghe@gmail.com>
2018-04-11 13:32:28 +0200

    tempMain

--- UI/src/main/java/figures/basicShapes/CloseButton.java
+++ UI/src/main/java/figures/basicShapes/CloseButton.java
@@ -1,4 +1,30 @@
 package figures.basicShapes;
 
-public class CloseButton {
+import java.awt.*;
+
+public class CloseButton extends Shape{
+
+    private int x,y,width = 21,height = 16;
+
+    /**
+     * @param x
+     *      the x-coordinate of the top-right corner of the subwindow
+     * @param y
+     *      the y-coordinate of the top-right corner of the subwindow
+     */
+    public CloseButton(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        new Rectangle(x-width, y, width, height).draw(graphics);
+        new Line(x-(int)Math.floor(width/3),y+(int)Math.floor(height/4),x-((int)Math.floor(width/3)*2),y+((int)Math.floor(height/4)*3)).draw(graphics);
+        new Line(x-((int)Math.floor(width/3)*2),y+((int)Math.floor(height/4)),x-(int)Math.floor(width/3),y+((int)Math.floor(height/4)*3)).draw(graphics);
+    }
 }
--- UI/src/main/java/figures/diagramFigures/SubwindowFigure.java
+++ UI/src/main/java/figures/diagramFigures/SubwindowFigure.java
@@ -1,4 +1,36 @@
 package figures.diagramFigures;
 
-public class SubwindowFigure {
+import figures.basicShapes.CloseButton;
+import figures.basicShapes.Line;
+import figures.basicShapes.Rectangle;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public class SubwindowFigure extends Figure{
+
+    private int x,y;
+
+    public SubwindowFigure(Point2D point){
+        x = (int)point.getX();
+        y = (int)point.getY();
+    }
+
+    @Override
+    public void draw(Graphics graphics) {
+        //graphics.setColor(Color.WHITE);
+        //graphics.fillRect(x,y,Subwindow.WIDTH,Subwindow.HEIGHT);
+        //graphics.setColor(Color.BLACK);
+        //new Rectangle(x,y, Subwindow.WIDTH, Subwindow.HEIGHT);
+        //closeButton = new CloseButton(x+Subwindow.WIDTH, y);
+        //new Line(x, y+closeButton.getHeight(),x+Subwindow.WIDTH, y+closeButton.getHeight());
+
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(x,y,300,300);
+        graphics.setColor(Color.BLACK);
+        new Rectangle(x,y, 300, 300).draw(graphics);
+        CloseButton closeButton = new CloseButton(x+300, y);
+        closeButton.draw(graphics);
+        new Line(x, y+closeButton.getHeight(),x+300, y+closeButton.getHeight()).draw(graphics);
+    }
 }
