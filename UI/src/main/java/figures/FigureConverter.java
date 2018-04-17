a9737b8e04ec4aa0eca5ca91a7b78e481c3bb0b1
AdamBoghe <adam.boghe@gmail.com>
2018-04-18 00:37:44 +0200

    sout removed

--- UI/src/main/java/figures/FigureConverter.java
+++ UI/src/main/java/figures/FigureConverter.java
@@ -58,7 +58,6 @@ public class FigureConverter {
         Subwindow sub;
         for (CanvasController.SubWindowLevel subLvl : subwindowLevels) {
             sub = subLvl.getSubwindow();
-            System.out.println(subwindowLevels.size()+" wtf: "+subLvl.getLevel());
             setConverters(sub);
             drawSubwindow(graphics, sub.getPosition(), sub.getWidth(), sub.getHeight());
 
