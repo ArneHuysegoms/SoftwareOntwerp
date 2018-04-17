83b108a0d104a13430f3dbfd0a2627feaaab4b59
Unknown <arhuy@hotmail.com>
2018-04-17 09:36:19 +0200

    getSelected()

--- UI/src/test/java/subwindow/SubwindowTest.java
+++ UI/src/test/java/subwindow/SubwindowTest.java
@@ -1,6 +1,7 @@
 package subwindow;
 
 import controller.CanvasController;
+import mediator.InteractionMediator;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,22 +12,35 @@ import static org.junit.Assert.assertTrue;
 
 public class SubwindowTest {
     private Subwindow subwindow;
+    private Button button;
+    private InteractionMediator interactionMediator;
+    private Point2D point;
 
-    /*@Before
+    @Before
     public void setUp(){
-        Point2D point =
-        subwindow = new Subwindow()
+        point = new Point2D.Double(200,200);
+        button = new CloseButton();
+        interactionMediator = new InteractionMediator();
+        subwindow = new Subwindow(point, button, interactionMediator);
     }
 
     @Test
-    public void test_width_button1(){
-        assertEquals(button1.getWidth(), 30);
+    public void test_subwindow_width(){
+        assertEquals(subwindow.getWidth(), 600);
     }
     @Test
-    public void test_width_button2(){
-        assertEquals(button2.getWidth(), 30);
+    public void test_subwindow_height(){
+        assertEquals(subwindow.getHeight(), 600);
     }
     @Test
+    public void test_subwindow_position(){
+        assertTrue(subwindow.getPosition().equals(point));
+    }
+    @Test
+    public void test_subwindow_button(){
+        assertTrue(subwindow.getButton().equals(button));
+    }
+   /* @Test
     public void test_height_button1(){
         assertEquals(button1.getHeight(), 30);
     }
@@ -42,5 +56,5 @@ public class SubwindowTest {
     public void test_controller_button2(){
         assertTrue(button2.getController().equals(this.canvasController));
     }
-
+*/
 }
