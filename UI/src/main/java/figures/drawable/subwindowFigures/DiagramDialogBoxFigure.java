549235c497cc86785cc4f426c377de11535859a9
AdamBoghe <adam.boghe@gmail.com>
2018-05-20 13:06:56 +0200

    TextBoxFigure made, PartyDialogBoxFigure finished

--- UI/src/main/java/figures/drawable/subwindowFigures/DiagramDialogBoxFigure.java
+++ UI/src/main/java/figures/drawable/subwindowFigures/DiagramDialogBoxFigure.java
@@ -1,19 +1,15 @@
 package figures.drawable.subwindowFigures;
 
-import figures.drawable.basicShapes.Circle;
-import figures.drawable.basicShapes.FilledCircle;
 import figures.drawable.basicShapes.RadioButtonFigure;
+import figures.drawable.basicShapes.SelectedRadioButtonFigure;
 import window.dialogbox.DiagramDialogBox;
-import window.elements.RadioButton;
 
 import java.awt.*;
-import java.awt.geom.Point2D;
-
-//TODO A dialog box for a diagram shows two radio buttons for choosing between a sequence diagram and a communication diagram.
 
 public class DiagramDialogBoxFigure extends SubwindowFigure {
 
     private DiagramDialogBox dialogBox;
+
     /**
      * @param dialogBox
      */
@@ -22,24 +18,25 @@ public class DiagramDialogBoxFigure extends SubwindowFigure {
         this.dialogBox = dialogBox;
     }
 
-
     @Override
     public void draw(Graphics graphics, int minX, int minY, int maxX, int maxY) {
         super.draw(graphics, minX, minY, maxX, maxY);
 
-        new RadioButtonFigure(dialogBox.getToCommunicationDiagram(),DiagramDialogBox.TOCOMMUNICATIONDIAGRAM_DESPCRIPTION)
-        .draw(graphics, minX, minY, maxX, maxY);
-        new RadioButtonFigure(dialogBox.getToSequenceDiagram(),DiagramDialogBox.TOSEQUENCEDIAGRAM_DESCRIPTION)
-        .draw(graphics, minX, minY, maxX, maxY);
+        new RadioButtonFigure(dialogBox.getToCommunicationDiagram(), DiagramDialogBox.TOCOMMUNICATIONDIAGRAM_DESPCRIPTION)
+                .draw(graphics, minX, minY, maxX, maxY);
+        new RadioButtonFigure(dialogBox.getToSequenceDiagram(), DiagramDialogBox.TOSEQUENCEDIAGRAM_DESCRIPTION)
+                .draw(graphics, minX, minY, maxX, maxY);
 
-       drawSelectedRadioButton(graphics, minX, minY, maxX, maxY)
-        new FilledCircle(dialogBox.getSelected().getCoordinate(), RadioButton.HEIGHT).draw(graphics, minX, minY, maxX, maxY);;
+        drawSelectedRadioButton(graphics, minX, minY, maxX, maxY);
     }
 
     private void drawSelectedRadioButton(Graphics graphics, int minX, int minY, int maxX, int maxY) {
-        //TODO NEXT
-        //if(dialogBox.getDiagramSubwindow() instanceof Communication)
+        if (dialogBox.getDiagramSubwindow().isCommunicationDiagram()) {
+            new SelectedRadioButtonFigure(dialogBox.getToCommunicationDiagram(), "")
+                    .draw(graphics, minX, minY, maxX, maxY);
+        } else if (dialogBox.getDiagramSubwindow().isSequenceDiagram()) {
+            new SelectedRadioButtonFigure(dialogBox.getToSequenceDiagram(),"")
+                    .draw(graphics, minX, minY, maxX, maxY);
+        }
     }
-
-
 }
