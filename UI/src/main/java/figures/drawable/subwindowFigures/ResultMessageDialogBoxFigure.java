d66b91d06d290f18fdef39b10a8bb4e4f47c94fa
AdamBoghe <adam.boghe@gmail.com>
2018-05-20 13:17:47 +0200

    TextboxeFigures used/implemented in PartyDBFigure & InvokeDBFigure & ResponseDBFigure

--- UI/src/main/java/figures/drawable/subwindowFigures/ResultMessageDialogBoxFigure.java
+++ UI/src/main/java/figures/drawable/subwindowFigures/ResultMessageDialogBoxFigure.java
@@ -1,5 +1,6 @@
 package figures.drawable.subwindowFigures;
 
+import figures.drawable.basicShapes.TextBoxFigure;
 import window.dialogbox.ResultMessageDialogBox;
 import window.elements.textbox.TextBox;
 
@@ -20,10 +21,8 @@ public class ResultMessageDialogBoxFigure extends SubwindowFigure {
         drawTextBox(graphics, minX, minY, maxX, maxY);
     }
 
-    public void drawTextBox(Graphics graphics, int minX, int minY, int maxX, int maxY){
-        //TODO description of static title dingetje toevoegen. EN .Wat er in de textBox sta/getypt wordt
-        new figures.drawable.basicShapes.Rectangle(dialogBox.getLabelTextBox().getCoordinate(), TextBox.HEIGHT, TextBox.WIDTH).draw(graphics, minX, minY, maxX, maxY);
+    public void drawTextBox(Graphics graphics, int minX, int minY, int maxX, int maxY) {
+        new TextBoxFigure(dialogBox.getLabelTextBox(), "??? ResultDBFigure")
+                .draw(graphics, minX, minY, maxX, maxY);
     }
-
-    //TODO A dialog box for a result message shows a text box for the label.
 }
