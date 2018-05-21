c37c0345447ee2412668125a4a28e311514e68ef
Unknown <arhuy@hotmail.com>
2018-05-21 21:23:41 +0200

    Tests command/actions

--- UI/src/test/java/command/CloseSubwindowCommandTest.java
+++ UI/src/test/java/command/CloseSubwindowCommandTest.java
@@ -7,7 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import window.Subwindow;
 import window.diagram.DiagramSubwindow;
+import window.elements.button.Button;
+import window.elements.button.CloseWindowButton;
 
+import java.awt.*;
 import java.awt.geom.Point2D;
 
 import static org.junit.Assert.assertTrue;
@@ -23,6 +26,15 @@ public class CloseSubwindowCommandTest {
         interactionController = new InteractionController();
         subwindow = new DiagramSubwindow(new Point2D.Double(100,100));
         closeSubwindowCommand = new CloseSubwindowCommand(subwindow, interactionController);
+        Button button = new CloseWindowButton(closeSubwindowCommand);
+        subwindow.getFrame().setButton(button);
+        interactionController.getSubwindows().add(subwindow);
+        interactionController.setActiveSubwindow(subwindow);
     }
 
+    @Test
+    public void test_performAction(){
+        closeSubwindowCommand.performAction();
+        assertTrue(interactionController.getActiveSubwindow() == null);
+    }
 }
