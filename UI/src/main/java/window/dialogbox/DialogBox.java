f708a1901b11bc3c450ef2e814acd149366f07ff
Sander Joos <sander.joos@skynet.be>
2018-05-22 13:40:13 +0200

    doc push before debugging

--- UI/src/main/java/window/dialogbox/DialogBox.java
+++ UI/src/main/java/window/dialogbox/DialogBox.java
@@ -9,15 +9,35 @@ import window.WindowLevelCounter;
 
 import java.awt.geom.Point2D;
 
+/**
+ * abstract superclass for dialogboxes
+ */
 public abstract class DialogBox extends Subwindow {
 
-    public DialogBox(Point2D pos){
-        super(pos, WindowLevelCounter.getNextLevel());
+    /**
+     * create a new dialogbox at the given position
+     *
+     * @param position the new position
+     */
+    public DialogBox(Point2D position) {
+        super(position, WindowLevelCounter.getNextLevel());
     }
 
+    /**
+     * handles a mouseEvent
+     *
+     * @param mouseEvent the mouseEvent to handle
+     * @return an action detailing the change by the mouseEvent
+     */
     @Override
     public abstract Action handleMouseEvent(MouseEvent mouseEvent);
 
+    /**
+     * handles a keyEvent
+     *
+     * @param keyEvent the keyEvent to handle
+     * @return an action detailing the change by the keyEvent
+     */
     @Override
     public abstract Action handleKeyEvent(KeyEvent keyEvent);
 }
