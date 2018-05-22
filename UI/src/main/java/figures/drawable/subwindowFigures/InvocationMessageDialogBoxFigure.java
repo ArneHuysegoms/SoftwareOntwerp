a5cf5458631697c3912f2401d3e950203607c376
AdamBoghe <adam.boghe@gmail.com>
2018-05-22 15:24:08 +0200

    invocation dialog box button's get grayed out

--- UI/src/main/java/figures/drawable/subwindowFigures/InvocationMessageDialogBoxFigure.java
+++ UI/src/main/java/figures/drawable/subwindowFigures/InvocationMessageDialogBoxFigure.java
@@ -16,6 +16,7 @@ public class InvocationMessageDialogBoxFigure extends SubwindowFigure {
 
     /**
      * draws an invocation message dialog box
+     *
      * @param graphics object used to draw on the program's window
      * @param minX     minimum possible x coördinate value
      * @param minY     minimum possible y coördinate value
@@ -26,24 +27,26 @@ public class InvocationMessageDialogBoxFigure extends SubwindowFigure {
     public void draw(Graphics graphics, int minX, int minY, int maxX, int maxY) {
         super.draw(graphics, minX, minY, maxX, maxY);
         drawTextBoxes(graphics, minX, minY, maxX, maxY);
-        drawButtons(graphics, minX, minY, maxX, maxY);
+        drawButtons(graphics, dialogBox.getArgumentListBox().getSelectedIndex(), minX, minY, maxX, maxY);
         drawListBox(graphics, minX, minY, maxX, maxY);
     }
 
     /**
      * draws an invocation message dialog box
+     *
      * @param graphics object used to draw on the program's window
      */
     @Override
     public void draw(Graphics graphics) {
         super.draw(graphics);
         drawTextBoxes(graphics);
-        drawButtons(graphics);
+        drawButtons(graphics, dialogBox.getArgumentListBox().getSelectedIndex());
         drawListBox(graphics);
     }
 
     /**
      * draws the list box on the dialog box
+     *
      * @param graphics object used to draw on the program's window
      * @param minX     minimum possible x coördinate value
      * @param minY     minimum possible y coördinate value
@@ -57,6 +60,7 @@ public class InvocationMessageDialogBoxFigure extends SubwindowFigure {
 
     /**
      * draws the list box on the dialog box
+     *
      * @param graphics object used to draw on the program's window
      */
     private void drawListBox(Graphics graphics) {
@@ -66,43 +70,54 @@ public class InvocationMessageDialogBoxFigure extends SubwindowFigure {
 
     /**
      * draws the buttons on the dialog box
+     *
      * @param graphics object used to draw on the program's window
      * @param minX     minimum possible x coördinate value
      * @param minY     minimum possible y coördinate value
      * @param maxX     maximum possible x coördinate value
      * @param maxY     maximum possible y coördinate value
      */
-    private void drawButtons(Graphics graphics, int minX, int minY, int maxX, int maxY) {
-
+    private void drawButtons(Graphics graphics, int selectedIndex, int minX, int minY, int maxX, int maxY) {
         new AddButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getAddArgument().getCoordinate()), dialogBox.getAddArgument().getWidth(), dialogBox.getAddArgument().getHeight())
                 .draw(graphics, minX, minY, maxX, maxY);
+        Color temp = graphics.getColor();
+        if (selectedIndex < 0) {
+            graphics.setColor(Color.LIGHT_GRAY);
+        }
         new RemoveButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getDeleteArgument().getCoordinate()), dialogBox.getDeleteArgument().getWidth(), dialogBox.getDeleteArgument().getHeight())
                 .draw(graphics, minX, minY, maxX, maxY);
         new UpButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getMoveUp().getCoordinate()), dialogBox.getMoveUp().getWidth(), dialogBox.getMoveUp().getHeight())
                 .draw(graphics, minX, minY, maxX, maxY);
         new DownButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getMoveDown().getCoordinate()), dialogBox.getMoveDown().getWidth(), dialogBox.getMoveDown().getHeight())
                 .draw(graphics, minX, minY, maxX, maxY);
-
+        graphics.setColor(temp);
     }
 
     /**
      * draws the buttons on the dialog box
+     *
      * @param graphics object used to draw on the program's window
      */
-    private void drawButtons(Graphics graphics) {
+    private void drawButtons(Graphics graphics, int selectedIndex) {
         new AddButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getAddArgument().getCoordinate()), dialogBox.getAddArgument().getWidth(), dialogBox.getAddArgument().getHeight())
                 .draw(graphics);
+
+        Color temp = graphics.getColor();
+        if (selectedIndex < 0) {
+            graphics.setColor(Color.LIGHT_GRAY);
+        }
         new RemoveButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getDeleteArgument().getCoordinate()), dialogBox.getDeleteArgument().getWidth(), dialogBox.getDeleteArgument().getHeight())
                 .draw(graphics);
         new UpButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getMoveUp().getCoordinate()), dialogBox.getMoveUp().getWidth(), dialogBox.getMoveUp().getHeight())
                 .draw(graphics);
         new DownButtonFigure(dialogBox.getAbsolutePosition(dialogBox.getMoveDown().getCoordinate()), dialogBox.getMoveDown().getWidth(), dialogBox.getMoveDown().getHeight())
                 .draw(graphics);
-
+        graphics.setColor(temp);
     }
 
     /**
      * draws the text boxes on the dialog box
+     *
      * @param graphics object used to draw on the program's window
      * @param minX     minimum possible x coördinate value
      * @param minY     minimum possible y coördinate value
@@ -118,6 +133,7 @@ public class InvocationMessageDialogBoxFigure extends SubwindowFigure {
 
     /**
      * draws the text boxes on the dialog box
+     *
      * @param graphics object used to draw on the program's window
      */
     private void drawTextBoxes(Graphics graphics) {
