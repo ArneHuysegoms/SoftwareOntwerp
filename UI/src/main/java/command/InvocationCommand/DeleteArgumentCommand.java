6d962f18a70c7b239f66d757e0547007cfe6067d
Adam Boghe <adam.boghe@gmail.com>
2018-08-15 10:19:38 +0200

    DeleteArgumentButton & DeleteArgumentCommand

---
+++ UI/src/main/java/command/InvocationCommand/DeleteArgumentCommand.java
@@ -0,0 +1,29 @@
+package command.InvocationCommand;
+
+import action.Action;
+import action.EmptyAction;
+import action.UpdateLabelAction;
+import command.Command;
+import diagram.label.InvocationMessageLabel;
+import window.diagram.DiagramSubwindow;
+import window.elements.ListBox;
+import window.elements.textbox.ArgumentTextBox;
+
+public class DeleteArgumentCommand extends Command {
+    private ListBox listBox;
+    private InvocationMessageLabel label;
+    private DiagramSubwindow subwindow;
+
+    public DeleteArgumentCommand(ListBox lb, InvocationMessageLabel iml, DiagramSubwindow diagramSubwindow){
+        this.listBox = lb;
+        this.label = iml;
+        this.subwindow = diagramSubwindow;
+    }
+
+    @Override
+    public Action performAction() {
+        label.deleteArgument(listBox.getSelectedIndex());
+        listBox.removeArgument();
+        return new UpdateLabelAction(subwindow.getFacade().findParentElement(label), label);
+    }
+}
