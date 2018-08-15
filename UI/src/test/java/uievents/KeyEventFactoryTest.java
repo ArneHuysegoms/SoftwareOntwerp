bd5744f7ef3556e5eb2852b02ddbc4433c9b0558
Unknown <arhuy@hotmail.com>
2018-08-15 17:58:31 +0200

    Auto stash before merge of "ArneDi" and "origin/ArneDi"
    KeyEventFactoryTest to 100%

--- UI/src/test/java/uievents/KeyEventFactoryTest.java
+++ UI/src/test/java/uievents/KeyEventFactoryTest.java
@@ -7,7 +7,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class KeyEventFactoryTest {
-    private KeyEvent keyEvent1, keyEvent2, keyEvent3, keyEvent4, keyEvent5, keyEvent6, keyEvent7, keyEvent8;
+    private KeyEvent keyEvent1, keyEvent2, keyEvent3, keyEvent4, keyEvent5, keyEvent6, keyEvent7, keyEvent8, keyEvent9, keyEvent10, keyEvent11,
+                keyEvent12, keyEvent13, keyEvent14, keyEvent15, keyEvent16;
     private KeyEventFactory keyEventFactory;
 
     @Before
@@ -21,6 +22,15 @@ public class KeyEventFactoryTest {
         keyEvent6 = new KeyEvent(KeyEventType.CHAR);
         keyEvent7 = new KeyEvent(KeyEventType.CTRLN);
         keyEvent8 = new KeyEvent(KeyEventType.CTRLD);
+        keyEvent9 = new KeyEvent(KeyEventType.CTRLE);
+        keyEvent10 = new KeyEvent(KeyEventType.CTRLENTER);
+        keyEvent11 = new KeyEvent(KeyEventType.ENTER);
+        keyEvent12 = new KeyEvent(KeyEventType.ARROWKEYLEFT);
+        keyEvent13 = new KeyEvent(KeyEventType.ARROWKEYUP);
+        keyEvent14 = new KeyEvent(KeyEventType.ARROWKEYRIGHT);
+        keyEvent15 = new KeyEvent(KeyEventType.ARROWKEYDOWN);
+        keyEvent16 = new KeyEvent(KeyEventType.SPACE);
+
     }
     @Test
     public void test_create_tab(){
@@ -63,6 +73,48 @@ public class KeyEventFactoryTest {
         keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 17, '0');
         KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 68, 'd');
         assertTrue(test.equals(keyEvent8));
+    }
+    @Test
+    public void test_create_ctrlE(){
+        keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 17, '0');
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 69, 'd');
+        assertTrue(test.equals(keyEvent9));
+    }
+    @Test
+    public void test_create_ctrlEnter(){
+        keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 17, '0');
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 10, 'd');
+        assertTrue(test.equals(keyEvent10));
+    }
+    @Test
+    public void test_create_Enter(){
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 10, 'd');
+        assertTrue(test.equals(keyEvent11));
+    }
+    @Test
+    public void test_create_ArrowKeyLEFT(){
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 37, 'd');
+        assertTrue(test.equals(keyEvent12));
+    }
+    @Test
+    public void test_create_ArrowKeyUP(){
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 38, 'd');
+        assertTrue(test.equals(keyEvent13));
+    }
+    @Test
+    public void test_create_ArrowKeyRIGHT(){
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 39, 'd');
+        assertTrue(test.equals(keyEvent14));
+    }
+    @Test
+    public void test_create_ArrowKeyDOWN(){
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 40, 'd');
+        assertTrue(test.equals(keyEvent15));
+    }
+    @Test
+    public void test_create_Space(){
+        KeyEvent test = keyEventFactory.createKeyEvent(java.awt.event.KeyEvent.KEY_PRESSED, 32, 'd');
+        assertTrue(test.equals(keyEvent16));
     }
     @Test
     public void test_compare_with_other_keyEvent(){
